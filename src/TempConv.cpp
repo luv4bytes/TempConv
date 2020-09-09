e58f89536accd2149747f809f6a737d4bce95cf1
luv4bytes <61706439+luv4bytes@users.noreply.github.com>
2020-09-09 18:05:25 +0200

    Copyright notice

--- src/TempConv.cpp
+++ src/TempConv.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2020 Lukas Pfeifer
+ * for TempConv version 1.0
+ *      https://github.com/luv4bytes/TempConv
+ *
+ * This file is part of TempConv.
+ *
+ * TempConv is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TempConv is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with TempConv. If not, see <http://www.gnu.org/licenses/>.
+ *
+*/
+
 #include "TempConv.h"
 
 double TempConv::Converter::Convert(UNITS inUnit, const double& inVal, UNITS outUnit)
