4c4d46fc33cdf1d3e60b59835b636c628812e695
luv4bytes <61706439+luv4bytes@users.noreply.github.com>
2020-09-13 12:05:22 +0200

    Conversion

--- include/TempConv.h
+++ include/TempConv.h
@@ -22,6 +22,8 @@
 #ifndef CONVERTER_H
 #define CONVERTER_H
 
+#include <map>
+
 namespace TempConv
 {
     class Converter
@@ -30,17 +32,6 @@ namespace TempConv
          *
          *  Following values are based on 1 degree celsius.
          */
-
-        /// Degree celsius
-        const double C = 1;
-        /// Kelvin
-        const double K = 274.15;
-        /// Degree Fahrenheit
-        const double F = 33.8;
-        /// Reaumur
-        const double R = 0.8;
-        /// Rankine
-        const double Rankine = 493.47;
 
     public:
 
@@ -58,6 +49,20 @@ namespace TempConv
             /// Rankine
             RANKINE
         };
+
+    private:
+
+        /// Unit mappings based on 1 degree celsius
+        std::map<UNITS, double> unitMap =
+        {
+            {CELSIUS, 1},
+            {KELVIN, 274.15},
+            {FAHRENHEIT, 33.8},
+            {REAUMUR, 0.8},
+            {RANKINE, 493.47}
+        };
+
+    public:
 
         /// Converts value 'inVal' of unit 'inUnit' to 'outUnit' and returns the value.
         /// @param inUnit The unit of the given value
--- src/TempConv.cpp
+++ src/TempConv.cpp
@@ -23,60 +23,8 @@
 
 double TempConv::Converter::Convert(UNITS inUnit, const double& inVal, UNITS outUnit)
 {
-    double celsius = 0.0;
-    double result = 0.0;
-
-    switch(inUnit)
-    {
-        case CELSIUS:
-            celsius = inVal;
-            break;
-
-        case KELVIN:
-            celsius = inVal / K;
-            break;
-
-        case FAHRENHEIT:
-            celsius = inVal / C;
-            break;
-
-        case REAUMUR:
-            celsius = inVal / R;
-            break;
-
-        case RANKINE:
-            celsius = inVal / Rankine;
-            break;
-
-        default:
-            break;
-    }
-
-    switch(outUnit)
-    {
-        case CELSIUS:
-            result = celsius;
-            break;
-
-        case KELVIN:
-            result = celsius * K;
-            break;
-
-        case FAHRENHEIT:
-            result = celsius * F;
-            break;
-
-        case REAUMUR:
-            result = celsius * R;
-            break;
-
-        case RANKINE:
-            result = celsius * Rankine;
-            break;
-
-        default:
-            break;
-    }
+    double celsius = inVal / unitMap[inUnit];
+    double result = celsius * unitMap[outUnit];
 
     return result;
 }
